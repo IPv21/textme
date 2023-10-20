5412adee1f4713ae2f7aa294108df4e54f0a9ae8
will sherman <willsherman771@gmail.com>
2023-10-20 12:34:23 -0400

    added logic to database and click events

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,25 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    //store triggered event for later
+    window.deferredPrompt = event;
+    //remove hidden class from the button
+    butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+     promptEvent.prompt();
+     window.deferredPrompt = null;
+        butInstall.style.display = 'none';
+ });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.addEventListener = null;
+});
